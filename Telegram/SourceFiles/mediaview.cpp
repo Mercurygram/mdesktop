Mercurygram/mdesktop

commit 2c44d0b1fe84ade7742c3711f4bb9497bca4ca2f

fixing mediaview fullscreen on X11

Telegram/SourceFiles/mediaview.cpp
@@ -137,7 +137,7 @@ MediaView::MediaView()
 	});
 	handleAuthSessionChange();
 
-	setWindowFlags(Qt::FramelessWindowHint);
+	setWindowFlags(Qt::FramelessWindowHint | Qt::MaximizeUsingFullscreenGeometryHint);
 	moveToScreen();
 	setAttribute(Qt::WA_NoSystemBackground, true);
 	setAttribute(Qt::WA_TranslucentBackground, true);
@@ -1777,7 +1777,7 @@ void MediaView::displayFinished() {
 	updateControls();
 	if (isHidden()) {
 		psUpdateOverlayed(this);
-		show();
+		showFullScreen();
 		psShowOverAll(this);
 		activateWindow();
 		QApplication::setActiveWindow(this);

commit f0eda0632c2c1c866c521b6c07b87f91f585cb2a

Fix dock and menu hiding on macOS.
Regression was introduced in 2c44d0b1fe.

Telegram/SourceFiles/mediaview.cpp
@@ -137,11 +137,11 @@ MediaView::MediaView()
 	});
 	handleAuthSessionChange();
 
-#ifdef OS_MAC_OLD
-	setWindowFlags(Qt::FramelessWindowHint);
-#else // OS_MAC_OLD
+#ifdef Q_OS_LINUX
 	setWindowFlags(Qt::FramelessWindowHint | Qt::MaximizeUsingFullscreenGeometryHint);
-#endif // OS_MAC_OLD
+#else // Q_OS_LINUX
+	setWindowFlags(Qt::FramelessWindowHint);
+#endif // Q_OS_LINUX
 	moveToScreen();
 	setAttribute(Qt::WA_NoSystemBackground, true);
 	setAttribute(Qt::WA_TranslucentBackground, true);
@@ -1781,7 +1781,11 @@ void MediaView::displayFinished() {
 	updateControls();
 	if (isHidden()) {
 		psUpdateOverlayed(this);
+#ifdef Q_OS_LINUX
 		showFullScreen();
+#else // Q_OS_LINUX
+		show();
+#endif // Q_OS_LINUX
 		psShowOverAll(this);
 		activateWindow();
 		QApplication::setActiveWindow(this);