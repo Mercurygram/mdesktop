Mercurygram/mdesktop

commit b3eadac474b4dc6864bd2d8448faad4f3a1981b0

Fix changelog dialogs list appearance.
Fixes #5678, fixes #5674.

Telegram/SourceFiles/history/history.cpp
@@ -2086,8 +2086,13 @@ void History::markFullyLoaded() {
 }
 
 void History::setLastMessage(HistoryItem *item) {
-	if (_lastMessage && *_lastMessage == item) {
-		return;
+	if (_lastMessage) {
+		if (*_lastMessage == item) {
+			return;
+		} else if (!IsServerMsgId((*_lastMessage)->id)
+				&& (*_lastMessage)->date() > item->date()) {
+			return;
+		}
 	}
 	_lastMessage = item;
 	_chatListMessage = std::nullopt;

commit af895657c08fb512b4e85f6a6646b63563404e25

Fix another crash in last message checking.
Regression was introduced in b3eadac474.

Telegram/SourceFiles/history/history.cpp
@@ -2090,6 +2090,7 @@ void History::setLastMessage(HistoryItem *item) {
 		if (*_lastMessage == item) {
 			return;
 		} else if (*_lastMessage
+			&& item
 			&& !IsServerMsgId((*_lastMessage)->id)
 			&& (*_lastMessage)->date() > item->date()) {
 			return;