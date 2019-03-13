Mercurygram/mdesktop

commit 6e5fe0da9952f602355aaf79fef18c93a7996440

Refactored counting idle time.  - psIdleTime() was replaced with Platform::LastUserInputTime().  - _lastTimeVideoPlayedAt was moved to Application as _lastNonIdleTime.  - Call of updateNonIdle() was added while voice is recording.  - Fixed #5695.  - Thanks Preston. =)

Telegram/SourceFiles/mainwidget.cpp
@@ -3767,7 +3767,7 @@ void MainWidget::updateOnline(bool gotOtherOffline) {
 	bool isOnline = !App::quitting() && App::wnd()->isActive();
 	int updateIn = Global::OnlineUpdatePeriod();
 	if (isOnline) {
-		auto idle = psIdleTime();
+		const auto idle = crl::now() - Platform::LastUserInputTime();
 		if (idle >= Global::OfflineIdleTimeout()) {
 			isOnline = false;
 			if (!_isIdle) {
@@ -3883,7 +3883,7 @@ void MainWidget::writeDrafts(History *history) {
 }
 
 void MainWidget::checkIdleFinish() {
-	if (psIdleTime() < Global::OfflineIdleTimeout()) {
+	if (crl::now() - Platform::LastUserInputTime() < Global::OfflineIdleTimeout()) {
 		_idleFinishTimer.cancel();
 		_isIdle = false;
 		updateOnline();

Telegram/SourceFiles/auth_session.cpp
@@ -483,11 +483,9 @@ void AuthSession::checkAutoLock() {
 	}
 
 	Core::App().checkLocalTime();
-	auto now = crl::now();
-	auto shouldLockInMs = Global::AutoLock() * 1000LL;
-	auto idleForMs = psIdleTime();
-	auto notPlayingVideoForMs = now - settings().lastTimeVideoPlayedAt();
-	auto checkTimeMs = qMin(idleForMs, notPlayingVideoForMs);
+	const auto now = crl::now();
+	const auto shouldLockInMs = Global::AutoLock() * 1000LL;
+	const auto checkTimeMs = now - Core::App().lastNonIdleTime();
 	if (checkTimeMs >= shouldLockInMs || (_shouldLockAt > 0 && now > _shouldLockAt + kAutoLockTimeoutLateMs)) {
 		Core::App().lockByPasscode();
 	} else {

Telegram/SourceFiles/auth_session.h
@@ -139,12 +139,6 @@ public:
 	bool smallDialogsList() const {
 		return _variables.smallDialogsList;
 	}
-	void setLastTimeVideoPlayedAt(crl::time time) {
-		_lastTimeVideoPlayedAt = time;
-	}
-	crl::time lastTimeVideoPlayedAt() const {
-		return _lastTimeVideoPlayedAt;
-	}
 	void setSoundOverride(const QString &key, const QString &path) {
 		_variables.soundOverrides.insert(key, path);
 	}
@@ -263,7 +257,6 @@ private:
 	rpl::event_stream<bool> _tabbedReplacedWithInfoValue;
 
 	Variables _variables;
-	crl::time _lastTimeVideoPlayedAt = 0;
 
 };
 

Telegram/SourceFiles/core/application.cpp
@@ -841,6 +841,14 @@ bool Application::passcodeLocked() const {
 	return _passcodeLock.current();
 }
 
+void Application::updateNonIdle() {
+	_lastNonIdleTime = crl::now();
+}
+
+crl::time Application::lastNonIdleTime() const {
+	return std::max(Platform::LastUserInputTime(), _lastNonIdleTime);
+}
+
 rpl::producer<bool> Application::passcodeLockChanges() const {
 	return _passcodeLock.changes();
 }

Telegram/SourceFiles/core/application.h
@@ -184,6 +184,9 @@ public:
 	rpl::producer<bool> lockChanges() const;
 	rpl::producer<bool> lockValue() const;
 
+	[[nodiscard]] crl::time lastNonIdleTime() const;
+	void updateNonIdle();
+
 	void registerLeaveSubscription(QWidget *widget);
 	void unregisterLeaveSubscription(QWidget *widget);
 
@@ -281,6 +284,8 @@ private:
 	std::vector<LeaveSubscription> _leaveSubscriptions;
 
 	rpl::lifetime _lifetime;
+
+	crl::time _lastNonIdleTime = 0;
 
 };
 

Telegram/SourceFiles/history/history_widget.cpp
@@ -1241,6 +1241,7 @@ void HistoryWidget::onRecordUpdate(quint16 level, qint32 samples) {
 	if (samples < 0 || samples >= Media::Player::kDefaultFrequency * AudioVoiceMsgMaxLength) {
 		stopRecording(_peer && samples > 0 && _inField);
 	}
+	Core::App().updateNonIdle();
 	updateField();
 	if (_history) {
 		updateSendAction(_history, SendAction::Type::RecordVoice);

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -2041,7 +2041,7 @@ void OverlayWidget::onVideoPlayProgress(const AudioMsgId &audioId) {
 		if (state.length) {
 			updateVideoPlaybackState(state);
 		}
-		Auth().settings().setLastTimeVideoPlayedAt(crl::now());
+		Core::App().updateNonIdle();
 	}
 }
 

Telegram/SourceFiles/platform/linux/specific_linux.cpp
@@ -207,10 +207,6 @@ bool psIdleSupported() {
 	return false;
 }
 
-crl::time psIdleTime() {
-	return crl::now() - _lastUserAction;
-}
-
 void psActivateProcess(uint64 pid) {
 //	objc_activateProgram();
 }
@@ -447,6 +443,10 @@ bool OpenSystemSettings(SystemSettingsType type) {
 		}) != end(options);
 	}
 	return true;
+}
+
+crl::time LastUserInputTime() {
+	return _lastUserAction;
 }
 
 namespace ThirdParty {

commit 724c60bef3d632756eefe3eb48392634833f58d2

Fix idle time checking in MainWidget.
Regression was introduced in 6e5fe0da99.

Fixes #5779, fixes #5780.

Telegram/SourceFiles/mainwidget.cpp
@@ -3680,7 +3680,7 @@ void MainWidget::updateOnline(bool gotOtherOffline) {
 	bool isOnline = !App::quitting() && App::wnd()->isActive();
 	int updateIn = Global::OnlineUpdatePeriod();
 	if (isOnline) {
-		const auto idle = crl::now() - Platform::LastUserInputTime();
+		const auto idle = crl::now() - Core::App().lastNonIdleTime();
 		if (idle >= Global::OfflineIdleTimeout()) {
 			isOnline = false;
 			if (!_isIdle) {
@@ -3796,7 +3796,8 @@ void MainWidget::writeDrafts(History *history) {
 }
 
 void MainWidget::checkIdleFinish() {
-	if (crl::now() - Platform::LastUserInputTime() < Global::OfflineIdleTimeout()) {
+	if (crl::now() - Core::App().lastNonIdleTime()
+		< Global::OfflineIdleTimeout()) {
 		_idleFinishTimer.cancel();
 		_isIdle = false;
 		updateOnline();