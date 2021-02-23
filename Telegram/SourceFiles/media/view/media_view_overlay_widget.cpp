Mercurygram/mdesktop

commit b8c6e2c5ba798ffb9c92369142a2b3a9cdf5bcf3

Close PiP when opening a loading video.
Fixes #9926.

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -2443,7 +2443,9 @@ bool OverlayWidget::initStreaming(bool continueStreaming) {
 void OverlayWidget::startStreamingPlayer() {
 	Expects(_streamed != nullptr);
 
-	if (_streamed->instance.player().playing()) {
+	if (!_streamed->instance.player().paused()
+		&& !_streamed->instance.player().finished()
+		&& !_streamed->instance.player().failed()) {
 		if (!_streamed->withSound) {
 			return;
 		}

commit c45c273ec48989f50584ab62cdbfa67868841508

Fix animated userpics playback in media viewer.
Regression was introduced in b8c6e2c5ba.

I hope it fixes #10288.

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -2495,19 +2495,21 @@ bool OverlayWidget::initStreaming(bool continueStreaming) {
 void OverlayWidget::startStreamingPlayer() {
 	Expects(_streamed != nullptr);
 
-	if (!_streamed->instance.player().paused()
-		&& !_streamed->instance.player().finished()
-		&& !_streamed->instance.player().failed()) {
+	const auto &player = _streamed->instance.player();
+	if (player.playing()) {
 		if (!_streamed->withSound) {
 			return;
 		}
+		_pip = nullptr;
+	} else if (!player.paused() && !player.finished() && !player.failed()) {
 		_pip = nullptr;
 	} else if (_pip && _streamed->withSound) {
 		return;
 	}
 
 	const auto position = _document
-		? _document->session().settings().mediaLastPlaybackPosition(_document->id)
+		? _document->session().settings().mediaLastPlaybackPosition(
+			_document->id)
 		: _photo
 		? _photo->videoStartPosition()
 		: 0;