telegramdesktop/tdesktop

commit b3d1602354dfa52cb00c77b88bbf39e34bc6ef58

Forward bar and unread counter added to top bar in OneColumn layout.

Telegram/SourceFiles/mainwidget.cpp
@@ -2264,7 +2264,7 @@ void MainWidget::ui_showPeerHistory(quint64 peerId, qint32 showAtMsgId, Ui::Show
 			if (!animationParams.oldContentCache.isNull()) {
 				_dialogs->showAnimated(back ? Window::SlideDirection::FromLeft : Window::SlideDirection::FromRight, animationParams);
 			} else {
-				_dialogs->show();
+				_dialogs->showFast();
 			}
 		}
 	} else {
@@ -2820,7 +2820,7 @@ void MainWidget::showAll() {
 			}
 		}
 		if (selectingPeer()) {
-			_dialogs->show();
+			_dialogs->showFast();
 			_history->hide();
 			if (_overview) _overview->hide();
 			if (_wideSection) _wideSection->hide();
@@ -2833,7 +2833,7 @@ void MainWidget::showAll() {
 			_history->show();
 			_history->updateControlsGeometry();
 		} else {
-			_dialogs->show();
+			_dialogs->showFast();
 			_history->hide();
 		}
 		if (!selectingPeer()) {
@@ -2854,7 +2854,7 @@ void MainWidget::showAll() {
 				_forwardConfirm = 0;
 			}
 		}
-		_dialogs->show();
+		_dialogs->showFast();
 		if (_overview) {
 			_overview->show();
 		} else if (_wideSection) {
@@ -2972,12 +2972,13 @@ bool MainWidget::needBackButton() {
 	return _overview || _wideSection || _history->peer();
 }
 
-void MainWidget::paintTopBar(Painter &p, float64 over, int32 decreaseWidth) {
+bool MainWidget::paintTopBar(Painter &p, float64 over, int32 decreaseWidth) {
 	if (_overview) {
-		_overview->paintTopBar(p, over, decreaseWidth);
+		return _overview->paintTopBar(p, over, decreaseWidth);
 	} else if (!_wideSection) {
-		_history->paintTopBar(p, over, decreaseWidth);
+		return _history->paintTopBar(p, over, decreaseWidth);
 	}
+	return false;
 }
 
 QRect MainWidget::getMembersShowAreaGeometry() const {

Telegram/Resources/basic.style
@@ -817,6 +817,7 @@ contactsScroll: flatScroll(boxScroll) {
 
 simpleCloseIcon: icon {{ "simple_close", #c7c7c7 }};
 simpleCloseIconOver: icon {{ "simple_close", #a3a3a3 }};
+dialogsForwardCancelIcon: icon {{ "simple_close", dialogsForwardFg }};
 
 boxPhotoPadding: margins(28px, 28px, 28px, 18px);
 boxPhotoCompressedPadding: margins(0px, 2px, 0px, 22px);

Telegram/Resources/colors.palette
@@ -143,6 +143,8 @@ dialogsVerifiedIconFgActive: dialogsVerifiedIconFgOver;
 dialogsSendingIconFgActive: dialogsSendingIconFgOver;
 dialogsSentIconFgActive: dialogsSentIconFgOver;
 
+dialogsForwardFg: #ffffff;
+
 // history
 topBarBg: windowBg;
 

Telegram/Resources/sample.tdesktop-theme
@@ -115,6 +115,7 @@ dialogsVerifiedIconBgActive: dialogsVerifiedIconBgOver;
 dialogsVerifiedIconFgActive: dialogsVerifiedIconFgOver;
 dialogsSendingIconFgActive: dialogsSendingIconFgOver;
 dialogsSentIconFgActive: dialogsSentIconFgOver;
+dialogsForwardFg: #ffffff;
 topBarBg: windowBg;
 emojiPanBg: windowBg;
 emojiPanCategories: #f7f7f7; // windowBg;

Telegram/SourceFiles/application.cpp
@@ -932,9 +932,7 @@ void AppClass::call_handleHistoryUpdate() {
 }
 
 void AppClass::call_handleUnreadCounterUpdate() {
-	if (auto w = App::wnd()) {
-		w->updateUnreadCounter();
-	}
+	Global::RefUnreadCounterUpdate().notify(true);
 }
 
 void AppClass::call_handleFileDialogQueue() {

Telegram/SourceFiles/dialogs/dialogs.style
@@ -184,3 +184,19 @@ dialogsUpdateButton: flatButton {
 	font: semiboldFont;
 	overFont: semiboldFont;
 }
+
+dialogsForwardHeight: 32px;
+dialogsForwardTextLeft: 35px;
+dialogsForwardTextTop: 6px;
+dialogsForwardCancel: IconButton {
+	width: 34px;
+	height: dialogsForwardHeight;
+
+	icon: dialogsForwardCancelIcon;
+	iconOver: dialogsForwardCancelIcon;
+
+	iconPosition: point(12px, 11px);
+	iconPositionDown: point(12px, 11px);
+}
+dialogsForwardFont: semiboldFont;
+dialogsForwardBg: windowActiveFill;

Mercurygram/mdesktop

commit 2372ed33d20cbc77c66f853cd4260d74840497ac

Fixed stuck of forward bar when using -sendpath argument.
Regression was introduced in b3d1602354.

Telegram/SourceFiles/dialogs/dialogs_widget.h
@@ -85,6 +85,8 @@ public:
 	void searchMessages(const QString &query, Key inChat = {});
 	void onSearchMore();
 
+	void updateForwardBar();
+
 	// Float player interface.
 	bool floatPlayerHandleWheelEvent(QEvent *e) override;
 	QRect floatPlayerAvailableRect() override;
@@ -155,7 +157,6 @@ private:
 	void updateSearchFromVisibility(bool fast = false);
 	void updateControlsGeometry();
 	void refreshFolderTopBar();
-	void updateForwardBar();
 	void checkUpdateStatus();
 	void changeOpenedFolder(Data::Folder *folder, anim::type animated);
 	QPixmap grabForFolderSlideAnimation();

Telegram/SourceFiles/mainwidget.cpp
@@ -660,6 +660,8 @@ void MainWidget::clearHider(not_null<Window::HistoryHider*> instance) {
 				_history->showAnimated(Window::SlideDirection::FromRight, animationParams);
 			}
 			floatPlayerCheckVisibility();
+		} else {
+			_dialogs->updateForwardBar();
 		}
 	}
 }