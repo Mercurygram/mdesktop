Mercurygram/mdesktop

commit 1ceeb6701ad50d15434192bde8ef61c6d7529f11

Fix volume slider in voice chats.

Telegram/SourceFiles/ui/widgets/continuous_sliders.cpp
@@ -260,7 +260,14 @@ void MediaSlider::paintEvent(QPaintEvent *e) {
 		: seekRect.height();
 	const auto from = 0;
 	const auto length = (horizontal ? width() : height());
-	const auto mid = qRound(from + value * length);
+	const auto alwaysSeekSize = horizontal
+		? _st.seekSize.width()
+		: _st.seekSize.height();
+	const auto mid = _alwaysDisplayMarker
+		? qRound(from
+			+ (alwaysSeekSize / 2.)
+			+ value * (length - alwaysSeekSize))
+		: qRound(from + value * length);
 	const auto till = std::max(mid, qRound(from + receivedTill * length));
 	const auto end = from + length;
 	const auto activeFg = disabled

commit dc1b897ef8b1e5d7662e27bca86e543dc55a4a3d

Fix crash in vertical sliders.
Regression was introduced in 1ceeb6701a.

Telegram/SourceFiles/ui/widgets/continuous_sliders.cpp
@@ -268,7 +268,9 @@ void MediaSlider::paintEvent(QPaintEvent *e) {
 			+ (alwaysSeekSize / 2.)
 			+ value * (length - alwaysSeekSize))
 		: qRound(from + value * length);
-	const auto till = std::max(mid, qRound(from + receivedTill * length));
+	const auto till = horizontal
+		? mid
+		: std::max(mid, qRound(from + receivedTill * length));
 	const auto end = from + length;
 	const auto activeFg = disabled
 		? _st.activeFgDisabled