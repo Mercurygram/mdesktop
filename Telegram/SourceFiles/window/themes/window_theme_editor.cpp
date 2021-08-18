Mercurygram/mdesktop

commit 4f146b024a3a90f43c69bc0eaf4133806f0394be

Fix -Wunused-variable warnings

Telegram/SourceFiles/window/themes/window_theme_editor.cpp
@@ -164,16 +164,13 @@ bool isValidColorValue(QLatin1String value) {
 		skipWhitespacesAndComments(data, end);
 		if (data == end) break;
 
-		auto foundName = base::parse::readName(data, end);
 		skipWhitespacesAndComments(data, end);
 		if (data == end || *data != ':') {
 			return "error";
 		}
 		++data;
 		skipWhitespacesAndComments(data, end);
-		auto valueStart = data;
 		auto value = readValue(data, end);
-		auto valueEnd = data;
 		if (value.size() == 0) {
 			return "error";
 		}

commit 2b6797285cc568ff4135f19111208a7a02512c3b

Fix creating a theme from a default one.
Regression was introduced in 4f146b024a.

Telegram/SourceFiles/window/themes/window_theme_editor.cpp
@@ -164,6 +164,7 @@ bool isValidColorValue(QLatin1String value) {
 		skipWhitespacesAndComments(data, end);
 		if (data == end) break;
 
+		[[maybe_unused]] auto foundName = base::parse::readName(data, end);
 		skipWhitespacesAndComments(data, end);
 		if (data == end || *data != ':') {
 			return "error";