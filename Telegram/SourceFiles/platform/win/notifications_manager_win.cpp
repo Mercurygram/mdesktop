Mercurygram/mdesktop

commit 0f7d32e09fa1987b34b908d33f56326e2365e092

One Window::Notifications system for all sessions.

Telegram/SourceFiles/platform/win/notifications_manager_win.cpp
@@ -14,6 +14,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "history/history.h"
 #include "core/application.h"
 #include "core/core_settings.h"
+#include "main/main_session.h"
 #include "mainwindow.h"
 
 #include <Shobjidl.h>
@@ -208,13 +209,17 @@ class ToastEventHandler final : public Implements<
 	DesktopToastDismissedEventHandler,
 	DesktopToastFailedEventHandler> {
 public:
+	using NotificationId = Manager::NotificationId;
+
 	// We keep a weak pointer to a member field of native notifications manager.
 	ToastEventHandler(
 		const std::shared_ptr<Manager*> &guarded,
-		const PeerId &peer,
-		MsgId msg)
+		PeerId peer,
+		MsgId msg,
+		UserId selfId)
 	: _peerId(peer)
 	, _msgId(msg)
+	, _selfId(selfId)
 	, _weak(guarded) {
 	}
 
@@ -227,8 +232,9 @@ public:
 
 	// DesktopToastActivatedEventHandler
 	IFACEMETHODIMP Invoke(_In_ IToastNotification *sender, _In_ IInspectable* args) {
-		performOnMainQueue([peerId = _peerId, msgId = _msgId](Manager *manager) {
-			manager->notificationActivated(peerId, msgId);
+		const auto my = myId();
+		performOnMainQueue([my](Manager *manager) {
+			manager->notificationActivated(my);
 		});
 		return S_OK;
 	}
@@ -243,8 +249,9 @@ public:
 			case ToastDismissalReason_UserCanceled:
 			case ToastDismissalReason_TimedOut:
 			default:
-				performOnMainQueue([peerId = _peerId, msgId = _msgId](Manager *manager) {
-					manager->clearNotification(peerId, msgId);
+				const auto my = myId();
+				performOnMainQueue([my](Manager *manager) {
+					manager->clearNotification(my);
 				});
 			break;
 			}
@@ -254,8 +261,9 @@ public:
 
 	// DesktopToastFailedEventHandler
 	IFACEMETHODIMP Invoke(_In_ IToastNotification *sender, _In_ IToastFailedEventArgs *e) {
-		performOnMainQueue([peerId = _peerId, msgId = _msgId](Manager *manager) {
-			manager->clearNotification(peerId, msgId);
+		const auto my = myId();
+		performOnMainQueue([my](Manager *manager) {
+			manager->clearNotification(my);
 		});
 		return S_OK;
 	}
@@ -293,9 +301,14 @@ public:
 	}
 
 private:
+	[[nodiscard]] NotificationId myId() const {
+		return { .peerId = _peerId, .msgId = _msgId, .selfId = _selfId };
+	}
+
 	ULONG _refCount = 0;
 	PeerId _peerId = 0;
 	MsgId _msgId = 0;
+	UserId _selfId = 0;
 	std::weak_ptr<Manager*> _weak;
 
 };
@@ -353,9 +366,10 @@ public:
 		bool hideReplyButton);
 	void clearAll();
 	void clearFromHistory(not_null<History*> history);
-	void beforeNotificationActivated(PeerId peerId, MsgId msgId);
-	void afterNotificationActivated(PeerId peerId, MsgId msgId);
-	void clearNotification(PeerId peerId, MsgId msgId);
+	void clearFromSession(not_null<Main::Session*> session);
+	void beforeNotificationActivated(NotificationId id);
+	void afterNotificationActivated(NotificationId id);
+	void clearNotification(NotificationId id);
 
 	~Private();
 
@@ -376,7 +390,7 @@ private:
 
 		ComPtr<IToastNotification> p;
 	};
-	QMap<PeerId, QMap<MsgId, NotificationPtr>> _notifications;
+	base::flat_map<FullPeer, base::flat_map<MsgId, NotificationPtr>> _notifications;
 
 };
 
@@ -414,8 +428,8 @@ void Manager::Private::clearAll() {
 	if (!_notifier) return;
 
 	auto temp = base::take(_notifications);
-	for_const (auto &notifications, temp) {
-		for_const (auto &notification, notifications) {
+	for (const auto &[key, notifications] : base::take(_notifications)) {
+		for (const auto &[msgId, notification] : notifications) {
 			_notifier->Hide(notification.p.Get());
 		}
 	}
@@ -424,32 +438,51 @@ void Manager::Private::clearAll() {
 void Manager::Private::clearFromHistory(not_null<History*> history) {
 	if (!_notifier) return;
 
-	auto i = _notifications.find(history->peer->id);
+	auto i = _notifications.find(FullPeer{
+		history->peer->id,
+		history->session().userId()
+	});
 	if (i != _notifications.cend()) {
-		auto temp = base::take(i.value());
+		auto temp = base::take(i->second);
 		_notifications.erase(i);
 
-		for_const (auto &notification, temp) {
+		for (const auto &[msgId, notification] : temp) {
 			_notifier->Hide(notification.p.Get());
 		}
 	}
 }
 
-void Manager::Private::beforeNotificationActivated(PeerId peerId, MsgId msgId) {
-	clearNotification(peerId, msgId);
+void Manager::Private::clearFromSession(not_null<Main::Session*> session) {
+	if (!_notifier) return;
+
+	const auto selfId = session->userId();
+	for (auto i = _notifications.begin(); i != _notifications.end();) {
+		if (i->first.selfId == selfId) {
+			const auto temp = base::take(i->second);
+			_notifications.erase(i);
+
+			for (const auto &[msgId, notification] : temp) {
+				_notifier->Hide(notification.p.Get());
+			}
+		}
+	}
 }
 
-void Manager::Private::afterNotificationActivated(PeerId peerId, MsgId msgId) {
+void Manager::Private::beforeNotificationActivated(NotificationId id) {
+	clearNotification(id);
+}
+
+void Manager::Private::afterNotificationActivated(NotificationId id) {
 	if (auto window = App::wnd()) {
 		SetForegroundWindow(window->psHwnd());
 	}
 }
 
-void Manager::Private::clearNotification(PeerId peerId, MsgId msgId) {
-	auto i = _notifications.find(peerId);
+void Manager::Private::clearNotification(NotificationId id) {
+	auto i = _notifications.find(FullPeer{ id.peerId, id.selfId });
 	if (i != _notifications.cend()) {
-		i.value().remove(msgId);
-		if (i.value().isEmpty()) {
+		i->second.remove(id.msgId);
+		if (i->second.empty()) {
 			_notifications.erase(i);
 		}
 	}
@@ -481,10 +514,10 @@ bool Manager::Private::showNotification(
 	hr = SetAudioSilent(toastXml.Get());
 	if (!SUCCEEDED(hr)) return false;
 
-	const auto key = hideNameAndPhoto
+	const auto userpicKey = hideNameAndPhoto
 		? InMemoryKey()
 		: peer->userpicUniqueKey(userpicView);
-	const auto userpicPath = _cachedUserpics.get(key, peer, userpicView);
+	const auto userpicPath = _cachedUserpics.get(userpicKey, peer, userpicView);
 	const auto userpicPathWide = QDir::toNativeSeparators(userpicPath).toStdWString();
 
 	hr = SetImageSrc(userpicPathWide.c_str(), toastXml.Get());
@@ -533,7 +566,11 @@ bool Manager::Private::showNotification(
 	if (!SUCCEEDED(hr)) return false;
 
 	EventRegistrationToken activatedToken, dismissedToken, failedToken;
-	ComPtr<ToastEventHandler> eventHandler(new ToastEventHandler(_guarded, peer->id, msgId));
+	ComPtr<ToastEventHandler> eventHandler(new ToastEventHandler(
+		_guarded,
+		peer->id,
+		msgId,
+		peer->session().userId()));
 
 	hr = toast->add_Activated(eventHandler.Get(), &activatedToken);
 	if (!SUCCEEDED(hr)) return false;
@@ -544,26 +581,34 @@ bool Manager::Private::showNotification(
 	hr = toast->add_Failed(eventHandler.Get(), &failedToken);
 	if (!SUCCEEDED(hr)) return false;
 
-	auto i = _notifications.find(peer->id);
+	const auto key = FullPeer{
+		peer->id,
+		peer->session().userId()
+	};
+	auto i = _notifications.find(key);
 	if (i != _notifications.cend()) {
-		auto j = i->find(msgId);
-		if (j != i->cend()) {
-			ComPtr<IToastNotification> notify = j->p;
-			i->erase(j);
+		auto j = i->second.find(msgId);
+		if (j != i->second.end()) {
+			ComPtr<IToastNotification> notify = j->second.p;
+			i->second.erase(j);
 			_notifier->Hide(notify.Get());
-			i = _notifications.find(peer->id);
+			i = _notifications.find(key);
 		}
 	}
 	if (i == _notifications.cend()) {
-		i = _notifications.insert(peer->id, QMap<MsgId, NotificationPtr>());
+		i = _notifications.emplace(
+			key,
+			base::flat_map<MsgId, NotificationPtr>()).first;
 	}
 	hr = _notifier->Show(toast.Get());
 	if (!SUCCEEDED(hr)) {
-		i = _notifications.find(peer->id);
-		if (i != _notifications.cend() && i->isEmpty()) _notifications.erase(i);
+		i = _notifications.find(key);
+		if (i != _notifications.cend() && i->second.empty()) {
+			_notifications.erase(i);
+		}
 		return false;
 	}
-	_notifications[peer->id].insert(msgId, toast);
+	i->second.emplace(msgId, toast);
 
 	return true;
 }
@@ -576,8 +621,8 @@ bool Manager::init() {
 	return _private->init();
 }
 
-void Manager::clearNotification(PeerId peerId, MsgId msgId) {
-	_private->clearNotification(peerId, msgId);
+void Manager::clearNotification(NotificationId id) {
+	_private->clearNotification(id);
 }
 
 Manager::~Manager() = default;
@@ -610,12 +655,16 @@ void Manager::doClearFromHistory(not_null<History*> history) {
 	_private->clearFromHistory(history);
 }
 
-void Manager::onBeforeNotificationActivated(PeerId peerId, MsgId msgId) {
-	_private->beforeNotificationActivated(peerId, msgId);
+void Manager::doClearFromSession(not_null<Main::Session*> session) {
+	_private->clearFromSession(session);
 }
 
-void Manager::onAfterNotificationActivated(PeerId peerId, MsgId msgId) {
-	_private->afterNotificationActivated(peerId, msgId);
+void Manager::onBeforeNotificationActivated(NotificationId id) {
+	_private->beforeNotificationActivated(id);
+}
+
+void Manager::onAfterNotificationActivated(NotificationId id) {
+	_private->afterNotificationActivated(id);
 }
 #endif // !__MINGW32__
 

Telegram/SourceFiles/apiwrap.cpp
@@ -2601,7 +2601,7 @@ void ApiWrap::applyNotifySettings(
 		}
 	} break;
 	}
-	_session->notifications().checkDelayed();
+	Core::App().notifications().checkDelayed();
 }
 
 void ApiWrap::gotStickerSet(uint64 setId, const MTPmessages_StickerSet &result) {

Telegram/SourceFiles/core/application.cpp
@@ -99,6 +99,7 @@ Application::Application(not_null<Launcher*> launcher)
 , _fallbackProductionConfig(
 	std::make_unique<MTP::Config>(MTP::Environment::Production))
 , _domain(std::make_unique<Main::Domain>(cDataFile()))
+, _notifications(std::make_unique<Window::Notifications::System>())
 , _langpack(std::make_unique<Lang::Instance>())
 , _langCloudManager(std::make_unique<Lang::CloudManager>(langpack()))
 , _emojiKeywords(std::make_unique<ChatHelpers::EmojiKeywords>())
@@ -114,6 +115,11 @@ Application::Application(not_null<Launcher*> launcher)
 	passcodeLockChanges(
 	) | rpl::start_with_next([=] {
 		_shouldLockAt = 0;
+	}, _lifetime);
+
+	passcodeLockChanges(
+	) | rpl::start_with_next([=] {
+		_notifications->updateAll();
 	}, _lifetime);
 
 	_domain->activeSessionChanges(

Telegram/SourceFiles/core/application.h
@@ -27,6 +27,12 @@ struct TermsLock;
 class Controller;
 } // namespace Window
 
+namespace Window {
+namespace Notifications {
+class System;
+} // namespace Notifications
+} // namespace Window
+
 namespace ChatHelpers {
 class EmojiKeywords;
 } // namespace ChatHelpers
@@ -100,6 +106,9 @@ public:
 
 	[[nodiscard]] Ui::Animations::Manager &animationManager() const {
 		return *_animationsManager;
+	}
+	[[nodiscard]] Window::Notifications::System &notifications() const {
+		return *_notifications;
 	}
 
 	// Windows interface.
@@ -294,6 +303,10 @@ private:
 	QPointer<Ui::BoxContent> _badProxyDisableBox;
 
 	const std::unique_ptr<Media::Audio::Instance> _audio;
+
+	// Notifications should be destroyed before _audio.
+	const std::unique_ptr<Window::Notifications::System> _notifications;
+
 	const QImage _logo;
 	const QImage _logoNoMargin;
 

Telegram/SourceFiles/data/data_histories.cpp
@@ -165,7 +165,7 @@ void Histories::readInboxTill(
 		}
 	});
 
-	history->session().notifications().clearIncomingFromHistory(history);
+	Core::App().notifications().clearIncomingFromHistory(history);
 
 	const auto needsRequest = history->readInboxTillNeedsRequest(tillId);
 	if (!needsRequest && !force) {

Telegram/SourceFiles/data/data_session.cpp
@@ -1110,7 +1110,7 @@ void Session::setupUserIsContactViewer() {
 
 Session::~Session() {
 	// Optimization: clear notifications before destroying items.
-	_session->notifications().clearAllFast();
+	Core::App().notifications().clearFromSession(_session);
 	clearLocalStorage();
 }
 
@@ -1980,7 +1980,7 @@ void Session::updateNotifySettingsLocal(not_null<PeerData*> peer) {
 		_mutedPeers.emplace(peer);
 		unmuteByFinishedDelayed(changesIn);
 		if (history) {
-			_session->notifications().clearIncomingFromHistory(history);
+			Core::App().notifications().clearIncomingFromHistory(history);
 		}
 	} else {
 		_mutedPeers.erase(peer);
@@ -3488,7 +3488,7 @@ void Session::removeChatListEntry(Dialogs::Key key) {
 		}
 	}
 	if (const auto history = key.history()) {
-		session().notifications().clearFromHistory(history);
+		Core::App().notifications().clearFromHistory(history);
 	}
 }
 

Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
@@ -15,6 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "history/history.h"
 #include "history/history_item.h"
 #include "core/shortcuts.h"
+#include "core/application.h"
 #include "ui/widgets/buttons.h"
 #include "ui/widgets/popup_menu.h"
 #include "ui/text/text_utilities.h"
@@ -133,7 +134,7 @@ InnerWidget::InnerWidget(
 
 	subscribe(session().downloaderTaskFinished(), [=] { update(); });
 
-	subscribe(session().notifications().settingsChanged(), [=](
+	subscribe(Core::App().notifications().settingsChanged(), [=](
 			Window::Notifications::ChangeType change) {
 		if (change == Window::Notifications::ChangeType::CountMessages) {
 			// Folder rows change their unread badge with this setting.

Telegram/SourceFiles/history/history.cpp
@@ -43,6 +43,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/image/image.h"
 #include "ui/text_options.h"
 #include "core/crash_reports.h"
+#include "core/application.h"
 #include "base/unixtime.h"
 #include "styles/style_dialogs.h"
 
@@ -671,7 +672,7 @@ void History::destroyMessage(not_null<HistoryItem*> item) {
 	}
 
 	owner().unregisterMessage(item);
-	session().notifications().clearFromItem(item);
+	Core::App().notifications().clearFromItem(item);
 
 	auto hack = std::unique_ptr<HistoryItem>(item.get());
 	const auto i = _messages.find(hack);
@@ -1290,7 +1291,7 @@ void History::newItemAdded(not_null<HistoryItem*> item) {
 		owner().notifyUnreadItemAdded(item);
 		const auto stillShow = item->showNotification();
 		if (stillShow) {
-			session().notifications().schedule(item);
+			Core::App().notifications().schedule(item);
 			if (!item->out() && item->unread()) {
 				if (unreadCountKnown()) {
 					setUnreadCount(unreadCount() + 1);
@@ -1741,7 +1742,7 @@ void History::inboxRead(MsgId upTo, std::optional<int> stillUnread) {
 	}
 
 	_firstUnreadView = nullptr;
-	session().notifications().clearIncomingFromHistory(this);
+	Core::App().notifications().clearIncomingFromHistory(this);
 }
 
 void History::inboxRead(not_null<const HistoryItem*> wasRead) {

commit caf21dd07a656ca1a66edc95bfff3208f9013b03

Fix clearing notifications from Action Center.
Regression was introduced in 0f7d32e09f.

Telegram/SourceFiles/platform/win/notifications_manager_win.cpp
@@ -437,7 +437,6 @@ void Manager::Private::clearAll() {
 		return;
 	}
 
-	auto temp = base::take(_notifications);
 	for (const auto &[key, notifications] : base::take(_notifications)) {
 		for (const auto &[msgId, notification] : notifications) {
 			_notifier.Hide(notification);