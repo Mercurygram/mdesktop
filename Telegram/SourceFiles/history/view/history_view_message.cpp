Mercurygram/mdesktop

commit b5034e842055e05b39631e562d59ea30f45851a3

Destroy comments button when switched off.

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -2785,6 +2785,8 @@ int Message::resizeContentGetHeight(int newWidth) {
 
 		if (item->repliesAreComments() || item->externalReply()) {
 			newHeight += st::historyCommentsButtonHeight;
+		} else {
+			_comments = nullptr;
 		}
 		newHeight += viewButtonHeight();
 	} else if (mediaDisplayed) {

commit 941236dffbaf60ba8af405daac891a3784522f9d

Fix crash after comments button destruction.
Regression was introduced in b5034e8420.

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -2790,8 +2790,9 @@ int Message::resizeContentGetHeight(int newWidth) {
 
 		if (item->repliesAreComments() || item->externalReply()) {
 			newHeight += st::historyCommentsButtonHeight;
-		} else {
+		} else if (_comments) {
 			_comments = nullptr;
+			checkHeavyPart();
 		}
 		newHeight += viewButtonHeight();
 	} else if (mediaDisplayed) {