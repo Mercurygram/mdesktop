Mercurygram/mdesktop

commit 86b91598d1a8d3afacbd553bb5b5de69b1b6c9c6

Extract BottomInfo from HistoryView::Message.

Telegram/SourceFiles/history/history_item.cpp
@@ -524,7 +524,7 @@ void HistoryItem::applySentMessage(const MTPDmessage &data) {
 	}, data.vmedia());
 	updateReplyMarkup(HistoryMessageMarkupData(data.vreply_markup()));
 	updateForwardedInfo(data.vfwd_from());
-	setViewsCount(data.vviews().value_or(-1));
+	changeViewsCount(data.vviews().value_or(-1));
 	if (const auto replies = data.vreplies()) {
 		setReplies(HistoryMessageRepliesData(replies));
 	} else {
@@ -541,6 +541,7 @@ void HistoryItem::applySentMessage(const MTPDmessage &data) {
 	setPostAuthor(data.vpost_author().value_or_empty());
 	contributeToSlowmode(data.vdate().v);
 	indexAsNewItem();
+	history()->owner().notifyItemDataChange(this);
 	history()->owner().requestItemTextRefresh(this);
 	history()->owner().updateDependentMessages(this);
 }
@@ -596,6 +597,7 @@ void HistoryItem::setRealId(MsgId newId) {
 		}
 	}
 
+	_history->owner().notifyItemDataChange(this);
 	_history->owner().requestItemRepaint(this);
 }
 
@@ -914,6 +916,7 @@ void HistoryItem::sendFailed() {
 	Expects(!(_flags & MessageFlag::SendingFailed));
 
 	_flags = (_flags | MessageFlag::SendingFailed) & ~MessageFlag::BeingSent;
+	_history->owner().notifyItemDataChange(this);
 	history()->session().changes().historyUpdated(
 		history(),
 		Data::HistoryUpdate::Flag::ClientSideMessages);

Telegram/SourceFiles/history/view/history_view_bottom_info.cpp
@@ -1,0 +1,303 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#include "history/view/history_view_bottom_info.h"
+
+#include "ui/chat/message_bubble.h"
+#include "ui/chat/chat_style.h"
+#include "ui/text/text_options.h"
+#include "lang/lang_keys.h"
+#include "history/history_item_components.h"
+#include "history/history_message.h"
+#include "history/view/history_view_message.h"
+#include "styles/style_chat.h"
+#include "styles/style_dialogs.h"
+
+namespace HistoryView {
+
+BottomInfo::BottomInfo(Data &&data) : _data(std::move(data)) {
+	layout();
+}
+
+void BottomInfo::update(Data &&data) {
+	_data = std::move(data);
+	layout();
+	if (!_size.isEmpty()) {
+		resizeToWidth(_size.width());
+	}
+}
+
+QSize BottomInfo::optimalSize() const {
+	return _optimalSize;
+}
+
+QSize BottomInfo::size() const {
+	return _size;
+}
+
+bool BottomInfo::pointInTime(QPoint position) const {
+	return QRect(
+		_size.width() - _dateWidth,
+		0,
+		_dateWidth,
+		st::msgDateFont->height
+	).contains(position);
+}
+
+bool BottomInfo::isSignedAuthorElided() const {
+	return _authorElided;
+}
+
+void BottomInfo::paint(
+		Painter &p,
+		QPoint position,
+		int outerWidth,
+		bool unread,
+		bool inverted,
+		const PaintContext &context) const {
+	const auto st = context.st;
+	const auto sti = context.imageStyle();
+	const auto stm = context.messageStyle();
+
+	auto right = position.x() + _size.width();
+	const auto firstLineBottom = position.y() + st::msgDateFont->height;
+	if (_data.flags & Data::Flag::OutLayout) {
+		const auto &icon = (_data.flags & Data::Flag::Sending)
+			? (inverted
+				? st->historySendingInvertedIcon()
+				: st->historySendingIcon())
+			: unread
+			? (inverted
+				? st->historySentInvertedIcon()
+				: stm->historySentIcon)
+			: (inverted
+				? st->historyReceivedInvertedIcon()
+				: stm->historyReceivedIcon);
+		icon.paint(
+			p,
+			QPoint(right, firstLineBottom) + st::historySendStatePosition,
+			outerWidth);
+		right -= st::historySendStateSpace;
+	}
+
+	const auto authorEditedWidth = _authorEditedDate.maxWidth();
+	right -= authorEditedWidth;
+	_authorEditedDate.drawLeft(
+		p,
+		right,
+		position.y(),
+		authorEditedWidth,
+		outerWidth);
+
+	if (!_views.isEmpty()) {
+		const auto viewsWidth = _views.maxWidth();
+		right -= st::historyViewsSpace + viewsWidth;
+		_views.drawLeft(p, right, position.y(), viewsWidth, outerWidth);
+
+		const auto &icon = inverted
+			? st->historyViewsInvertedIcon()
+			: stm->historyViewsIcon;
+		icon.paint(
+			p,
+			right - st::historyViewsWidth,
+			firstLineBottom + st::historyViewsTop,
+			outerWidth);
+	}
+	if (!_replies.isEmpty()) {
+		const auto repliesWidth = _replies.maxWidth();
+		right -= st::historyViewsSpace + repliesWidth;
+		_replies.drawLeft(p, right, position.y(), repliesWidth, outerWidth);
+
+		const auto &icon = inverted
+			? st->historyRepliesInvertedIcon()
+			: stm->historyRepliesIcon;
+		icon.paint(
+			p,
+			right - st::historyViewsWidth,
+			firstLineBottom + st::historyViewsTop,
+			outerWidth);
+	}
+	if ((_data.flags & Data::Flag::Sending)
+		&& !(_data.flags & Data::Flag::OutLayout)) {
+		right -= st::historySendStateSpace;
+		const auto &icon = inverted
+			? st->historyViewsSendingInvertedIcon()
+			: st->historyViewsSendingIcon();
+		icon.paint(
+			p,
+			right,
+			firstLineBottom + st::historyViewsTop,
+			outerWidth);
+	}
+	if (!_reactions.isEmpty()) {
+		if (_size.height() == _optimalSize.height()) {
+			_reactions.drawLeft(
+				p,
+				position.x(),
+				position.y(),
+				_reactions.maxWidth(),
+				outerWidth);
+		} else {
+			const auto available = _size.width();
+			const auto use = std::min(available, _reactions.maxWidth());
+			_reactions.drawLeftElided(
+				p,
+				position.x() + _size.width() - use,
+				position.y() + st::msgDateFont->height,
+				use,
+				outerWidth);
+		}
+	}
+}
+
+int BottomInfo::resizeToWidth(int newWidth) {
+	if (newWidth >= _optimalSize.width()) {
+		_size = _optimalSize;
+		return _size.height();
+	}
+	return 2 * st::msgDateFont->height;
+}
+
+void BottomInfo::layout() {
+	layoutDateText();
+	layoutViewsText();
+	layoutReactionsText();
+	countOptimalSize();
+}
+
+void BottomInfo::layoutDateText() {
+	const auto edited = (_data.flags & Data::Flag::Edited)
+		? (tr::lng_edited(tr::now) + ' ')
+		: QString();
+	const auto author = _data.author;
+	const auto prefix = author.isEmpty() ? qsl(", ") : QString();
+	const auto date = _data.date.toString(cTimeFormat());
+	_dateWidth = st::msgDateFont->width(date);
+	const auto afterAuthor = prefix + edited + date;
+	const auto afterAuthorWidth = st::msgDateFont->width(afterAuthor);
+	const auto authorWidth = st::msgDateFont->width(author);
+	const auto maxWidth = st::maxSignatureSize;
+	_authorElided = !author.isEmpty()
+		&& (authorWidth + afterAuthorWidth > maxWidth);
+	const auto name = _authorElided
+		? st::msgDateFont->elided(author, maxWidth - afterAuthorWidth)
+		: author;
+	const auto full = name + date;
+	_authorEditedDate.setText(
+		st::msgDateTextStyle,
+		full,
+		Ui::NameTextOptions());
+}
+
+void BottomInfo::layoutViewsText() {
+	if (!_data.views || (_data.flags & Data::Flag::Sending)) {
+		_views.clear();
+		return;
+	}
+	_views.setText(
+		st::msgDateTextStyle,
+		Lang::FormatCountToShort(std::max(*_data.views, 1)).string,
+		Ui::NameTextOptions());
+}
+
+void BottomInfo::layoutRepliesText() {
+	if (!_data.replies
+		|| !*_data.replies
+		|| (_data.flags & Data::Flag::RepliesContext)
+		|| (_data.flags & Data::Flag::Sending)) {
+		_replies.clear();
+		return;
+	}
+	_replies.setText(
+		st::msgDateTextStyle,
+		Lang::FormatCountToShort(*_data.replies).string,
+		Ui::NameTextOptions());
+}
+
+void BottomInfo::layoutReactionsText() {
+	if (_data.reactions.empty()) {
+		_reactions.clear();
+		return;
+	}
+	auto sorted = ranges::view::all(
+		_data.reactions
+	) | ranges::view::transform([](const auto &pair) {
+		return std::make_pair(pair.first, pair.second);
+	}) | ranges::to_vector;
+	ranges::sort(sorted, std::greater<>(), &std::pair<QString, int>::second);
+
+	auto fullCount = 0;
+	auto text = QString();
+	for (const auto &[string, count] : sorted) {
+		text.append(string);
+		fullCount += count;
+	}
+	text += QString::number(fullCount);
+
+	_reactions.setText(st::msgDateTextStyle, text, Ui::NameTextOptions());
+}
+
+void BottomInfo::countOptimalSize() {
+	auto width = 0;
+	if (_data.flags & (Data::Flag::OutLayout | Data::Flag::Sending)) {
+		width += st::historySendStateSpace;
+	}
+	width += _authorEditedDate.maxWidth();
+	if (!_views.isEmpty()) {
+		width += st::historyViewsSpace
+			+ _views.maxWidth()
+			+ st::historyViewsWidth;
+	}
+	if (!_reactions.isEmpty()) {
+		width += st::historyReactionsSkip + _reactions.maxWidth();
+	}
+	_optimalSize = QSize(width, st::msgDateFont->height);
+}
+
+BottomInfo::Data BottomInfoDataFromMessage(not_null<Message*> message) {
+	using Flag = BottomInfo::Data::Flag;
+
+	auto result = BottomInfo::Data();
+
+	const auto item = message->message();
+	result.date = message->dateTime();
+	result.reactions = item->reactions();
+	if (message->hasOutLayout()) {
+		result.flags |= Flag::OutLayout;
+	}
+	if (message->context() == Context::Replies) {
+		result.flags |= Flag::RepliesContext;
+	}
+	if (const auto msgsigned = item->Get<HistoryMessageSigned>()) {
+		 if (!msgsigned->isAnonymousRank) {
+			result.author = msgsigned->author;
+		 }
+	}
+	if (!item->hideEditedBadge()) {
+		if (const auto edited = message->displayedEditBadge()) {
+			result.flags |= Flag::Edited;
+		}
+	}
+	if (const auto views = item->Get<HistoryMessageViews>()) {
+		if (views->views.count >= 0) {
+			result.views = views->views.count;
+		}
+		if (views->replies.count >= 0 && !views->commentsMegagroupId) {
+			result.replies = views->replies.count;
+		}
+	}
+	if (item->isSending() || item->hasFailed()) {
+		result.flags |= Flag::Sending;
+	}
+	// We don't want to pass and update it in Date for now.
+	//if (item->unread()) {
+	//	result.flags |= Flag::Unread;
+	//}
+	return result;
+}
+
+} // namespace HistoryView

Telegram/SourceFiles/history/view/history_view_bottom_info.h
@@ -1,0 +1,85 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#pragma once
+
+#include "ui/text/text.h"
+#include "base/flags.h"
+
+namespace Ui {
+struct ChatPaintContext;
+} // namespace Ui
+
+namespace HistoryView {
+
+using PaintContext = Ui::ChatPaintContext;
+
+class Message;
+
+class BottomInfo {
+public:
+	struct Data {
+		enum class Flag {
+			Edited         = 0x01,
+			OutLayout      = 0x02,
+			Sending        = 0x04,
+			RepliesContext = 0x08,
+			//Unread, // We don't want to pass and update it in Date for now.
+		};
+		friend inline constexpr bool is_flag_type(Flag) { return true; };
+		using Flags = base::flags<Flag>;
+
+		QDateTime date;
+		QString author;
+		base::flat_map<QString, int> reactions;
+		std::optional<int> views;
+		std::optional<int> replies;
+		Flags flags;
+	};
+	explicit BottomInfo(Data &&data);
+
+	void update(Data &&data);
+
+	[[nodiscard]] QSize optimalSize() const;
+	[[nodiscard]] QSize size() const;
+	[[nodiscard]] bool pointInTime(QPoint position) const;
+	[[nodiscard]] bool isSignedAuthorElided() const;
+
+	void paint(
+		Painter &p,
+		QPoint position,
+		int outerWidth,
+		bool unread,
+		bool inverted,
+		const PaintContext &context) const;
+
+	int resizeToWidth(int newWidth);
+
+private:
+	void layout();
+	void layoutDateText();
+	void layoutViewsText();
+	void layoutRepliesText();
+	void layoutReactionsText();
+	void countOptimalSize();
+
+	Data _data;
+	QSize _optimalSize;
+	QSize _size;
+	Ui::Text::String _authorEditedDate;
+	Ui::Text::String _views;
+	Ui::Text::String _replies;
+	Ui::Text::String _reactions;
+	int _dateWidth = 0;
+	bool _authorElided = false;
+
+};
+
+[[nodiscard]] BottomInfo::Data BottomInfoDataFromMessage(
+	not_null<Message*> message);
+
+} // namespace HistoryView

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -237,30 +237,12 @@ LogEntryOriginal &LogEntryOriginal::operator=(LogEntryOriginal &&other) {
 
 LogEntryOriginal::~LogEntryOriginal() = default;
 
-void Reactions::update(const base::flat_map<QString, int> &list) {
-	auto sorted = ranges::view::all(
-		list
-	) | ranges::view::transform([](const auto &pair) {
-		return std::make_pair(pair.first, pair.second);
-	}) | ranges::to_vector;
-	ranges::sort(sorted, std::greater<>(), &std::pair<QString, int>::second);
-
-	auto fullCount = 0;
-	auto composed = QString();
-	for (const auto &[string, count] : sorted) {
-		composed.append(string);
-		fullCount += count;
-	}
-	composed += QString::number(fullCount);
-
-	text.setText(st::msgDateTextStyle, composed, Ui::NameTextOptions());
-}
-
 Message::Message(
 	not_null<ElementDelegate*> delegate,
 	not_null<HistoryMessage*> data,
 	Element *replacing)
-: Element(delegate, data, replacing) {
+: Element(delegate, data, replacing)
+, _bottomInfo(BottomInfoDataFromMessage(this)) {
 	initLogEntryOriginal();
 	initPsa();
 }
@@ -334,16 +316,8 @@ QSize Message::performCountOptimalSize() {
 
 	updateViewButtonExistence();
 	updateMediaInBubbleState();
-	refreshEditedBadge();
 	refreshRightBadge();
-
-	if (const auto list = item->reactions(); !list.empty()) {
-		AddComponents(Reactions::Bit());
-		const auto reactions = Get<Reactions>();
-		reactions->update(list);
-	} else {
-		RemoveComponents(Reactions::Bit());
-	}
+	initTime();
 
 	if (drawBubble()) {
 		const auto forwarded = item->Get<HistoryMessageForwarded>();
@@ -1776,111 +1750,25 @@ void Message::drawInfo(
 	break;
 	}
 
-	const auto item = message();
-	auto infoW = infoWidth();
-	if (rtl()) infoRight = width - infoRight + infoW;
-
-	auto dateX = infoRight - infoW;
-	auto dateY = infoBottom - st::msgDateFont->height;
+	const auto size = _bottomInfo.size();
+	const auto dateX = infoRight - size.width();
+	const auto dateY = infoBottom - size.height();
 	if (type == InfoDisplayType::Image) {
-		auto dateW = infoW + 2 * st::msgDateImgPadding.x(), dateH = st::msgDateFont->height + 2 * st::msgDateImgPadding.y();
+		const auto dateW = size.width() + 2 * st::msgDateImgPadding.x();
+		const auto dateH = size.height() + 2 * st::msgDateImgPadding.y();
 		Ui::FillRoundRect(p, dateX - st::msgDateImgPadding.x(), dateY - st::msgDateImgPadding.y(), dateW, dateH, sti->msgDateImgBg, sti->msgDateImgBgCorners);
 	} else if (type == InfoDisplayType::Background) {
-		auto dateW = infoW + 2 * st::msgDateImgPadding.x(), dateH = st::msgDateFont->height + 2 * st::msgDateImgPadding.y();
+		const auto dateW = size.width() + 2 * st::msgDateImgPadding.x();
+		const auto dateH = size.height() + 2 * st::msgDateImgPadding.y();
 		Ui::FillRoundRect(p, dateX - st::msgDateImgPadding.x(), dateY - st::msgDateImgPadding.y(), dateW, dateH, sti->msgServiceBg, sti->msgServiceBgCorners);
 	}
-	dateX += timeLeft();
-
-	if (const auto msgsigned = item->Get<HistoryMessageSigned>()
-		; msgsigned && !msgsigned->isAnonymousRank) {
-		msgsigned->signature.drawElided(p, dateX, dateY, item->_timeWidth);
-	} else if (const auto sponsored = displayedSponsorBadge()) {
-		const auto skipY = viewButtonHeight();
-		sponsored->text.drawElided(p, dateX, dateY - skipY, item->_timeWidth);
-	} else if (const auto edited = displayedEditBadge()) {
-		edited->text.drawElided(p, dateX, dateY, item->_timeWidth);
-	} else {
-		p.drawText(dateX, dateY + st::msgDateFont->ascent, item->_timeText);
-	}
-
-	const auto viewIconTop = infoBottom + st::historyViewsTop;
-	const auto pinIconTop = infoBottom + st::historyPinTop;
-	auto left = infoRight - infoW;
-	if (const auto reactions = Get<Reactions>()) {
-		reactions->text.draw(p, left, dateY, reactions->text.maxWidth());
-		left += reactions->text.maxWidth() + st::historyReactionsSkip;
-	}
-	if (const auto views = item->Get<HistoryMessageViews>()) {
-		const auto textTop = infoBottom - st::msgDateFont->descent;
-		if (views->replies.count > 0
-			&& !views->commentsMegagroupId
-			&& this->context() != Context::Replies) {
-			const auto &icon = (!item->isSending() && !item->hasFailed())
-				? (invertedsprites
-					? st->historyRepliesInvertedIcon()
-					: stm->historyRepliesIcon)
-				: (invertedsprites
-					? st->historyViewsSendingInvertedIcon()
-					: st->historyViewsSendingIcon());
-			if (!item->isSending() && !item->hasFailed()) {
-				icon.paint(p, left, viewIconTop, width);
-				p.drawText(left + st::historyViewsWidth, textTop, views->replies.text);
-			} else if (!context.outbg && views->views.count < 0) { // sending outbg icon will be painted below
-				auto iconSkip = st::historyViewsSpace + views->replies.textWidth;
-				icon.paint(p, left + iconSkip, viewIconTop, width);
-			}
-			left += st::historyViewsSpace
-				+ views->replies.textWidth
-				+ st::historyViewsWidth;
-		}
-		if (views->views.count >= 0) {
-			const auto &icon = (!item->isSending() && !item->hasFailed())
-				? (invertedsprites
-					? st->historyViewsInvertedIcon()
-					: stm->historyViewsIcon)
-				: (invertedsprites
-					? st->historyViewsSendingInvertedIcon()
-					: st->historyViewsSendingIcon());
-			if (!item->isSending() && !item->hasFailed()) {
-				icon.paint(p, left, viewIconTop, width);
-				p.drawText(left + st::historyViewsWidth, textTop, views->views.text);
-			} else if (!context.outbg) { // sending outbg icon will be painted below
-				auto iconSkip = st::historyViewsSpace + views->views.textWidth;
-				icon.paint(p, left + iconSkip, viewIconTop, width);
-			}
-			left += st::historyViewsSpace
-				+ views->views.textWidth
-				+ st::historyViewsWidth;
-		}
-	} else if ((item->isSending() || item->hasFailed())
-		&& item->history()->peer->isSelf()
-		&& !context.outbg) {
-		const auto &icon = invertedsprites
-			? st->historyViewsSendingInvertedIcon()
-			: st->historyViewsSendingIcon();
-		icon.paint(p, left, viewIconTop, width);
-	}
-	if (displayPinIcon()) {
-		const auto &icon = invertedsprites
-				? st->historyPinInvertedIcon()
-				: stm->historyPinIcon;
-		icon.paint(p, left, pinIconTop, width);
-		left += st::historyPinWidth;
-	}
-	if (context.outbg) {
-		const auto &icon = (item->isSending() || item->hasFailed())
-			? (invertedsprites
-				? st->historySendingInvertedIcon()
-				: st->historySendingIcon())
-			: delegate()->elementShownUnread(this)
-			? (invertedsprites
-					? st->historySentInvertedIcon()
-					: stm->historySentIcon)
-			: (invertedsprites
-					? st->historyReceivedInvertedIcon()
-					: stm->historyReceivedIcon);
-		icon.paint(p, QPoint(infoRight, infoBottom) + st::historySendStatePosition, width);
-	}
+	_bottomInfo.paint(
+		p,
+		{ dateX, dateY },
+		width,
+		delegate()->elementShownUnread(this),
+		invertedsprites,
+		context);
 }
 
 bool Message::pointInTime(
@@ -1904,53 +1792,28 @@ bool Message::pointInTime(
 		infoBottom -= st::msgDateImgPadding.y();
 		break;
 	}
-	const auto item = message();
-	auto dateX = infoRight - infoWidth() + timeLeft();
-	auto dateY = infoBottom - st::msgDateFont->height;
-	return QRect(
-		dateX,
-		dateY,
-		item->_timeWidth,
-		st::msgDateFont->height).contains(point);
+	const auto size = _bottomInfo.size();
+	const auto infoLeft = infoRight - size.width();
+	const auto infoTop = infoBottom - size.height();
+	return _bottomInfo.pointInTime({ infoLeft, infoTop });
 }
 
 int Message::infoWidth() const {
-	const auto item = message();
-	auto result = item->_timeWidth;
-	if (const auto views = item->Get<HistoryMessageViews>()) {
-		if (views->views.count >= 0) {
-			result += st::historyViewsSpace
-				+ views->views.textWidth
-				+ st::historyViewsWidth;
-		}
-		if (views->replies.count > 0
-			&& !views->commentsMegagroupId
-			&& context() != Context::Replies) {
-			result += st::historyViewsSpace
-				+ views->replies.textWidth
-				+ st::historyViewsWidth;
-		}
-	} else if ((item->isSending() || item->hasFailed())
-		&& item->history()->peer->isSelf()) {
-		if (!hasOutLayout()) {
-			result += st::historySendStateSpace;
-		}
-	}
-	if (displayPinIcon()) {
-		result += st::historyPinWidth;
-	}
+	return _bottomInfo.optimalSize().width();
+}
 
-	// When message is scheduled until online, time is not displayed,
-	// so message should have less space.
-	if (!item->_timeWidth) {
-		result += st::historyScheduledUntilOnlineStateSpace;
-	} else if (hasOutLayout()) {
-		result += st::historySendStateSpace;
+bool Message::isSignedAuthorElided() const {
+	return _bottomInfo.isSignedAuthorElided();
+}
+
+void Message::itemDataChanged() {
+	const auto was = _bottomInfo.size();
+	_bottomInfo.update(BottomInfoDataFromMessage(this));
+	if (was != _bottomInfo.size()) {
+		history()->owner().requestViewResize(this);
+	} else {
+		history()->owner().requestViewRepaint(this);
 	}
-	if (const auto reactions = Get<Reactions>()) {
-		result += st::historyReactionsSkip + reactions->text.maxWidth();
-	}
-	return result;
 }
 
 auto Message::verticalRepaintRange() const -> VerticalRepaintRange {
@@ -1972,34 +1835,6 @@ void Message::refreshDataIdHook() {
 	if (_comments) {
 		_comments->link = nullptr;
 	}
-}
-
-int Message::timeLeft() const {
-	const auto item = message();
-	auto result = 0;
-	if (auto views = item->Get<HistoryMessageViews>()) {
-		if (views->views.count >= 0) {
-			result += st::historyViewsSpace + views->views.textWidth + st::historyViewsWidth;
-		}
-		if (views->replies.count > 0
-			&& !views->commentsMegagroupId
-			&& context() != Context::Replies) {
-			result += st::historyViewsSpace + views->replies.textWidth + st::historyViewsWidth;
-		}
-	} else if ((item->isSending() || item->hasFailed())
-		&& item->history()->peer->isSelf()) {
-		if (!hasOutLayout()) {
-			result += st::historySendStateSpace;
-		}
-	}
-	if (displayPinIcon()) {
-		result += st::historyPinWidth;
-	}
-
-	if (const auto reactions = Get<Reactions>()) {
-		result += st::historyReactionsSkip + reactions->text.maxWidth();
-	}
-	return result;
 }
 
 int Message::plainMaxWidth() const {
@@ -2629,6 +2464,8 @@ int Message::resizeContentGetHeight(int newWidth) {
 			}
 		}
 	}
+	_bottomInfo.resizeToWidth(
+		std::min(_bottomInfo.optimalSize().width(), contentWidth));
 
 	if (bubble) {
 		auto reply = displayedReply();
@@ -2733,46 +2570,8 @@ QSize Message::performCountCurrentSize(int newWidth) {
 	return { newWidth, newHeight };
 }
 
-void Message::refreshEditedBadge() {
-	const auto item = message();
-	const auto edited = displayedEditBadge();
-	const auto editDate = displayedEditDate();
-	const auto dateText = dateTime().toString(cTimeFormat());
-	if (edited) {
-		edited->refresh(dateText, editDate != 0);
-	}
-	if (const auto msgsigned = item->Get<HistoryMessageSigned>()) {
-		if (!msgsigned->isAnonymousRank) {
-			const auto text = (!edited || !editDate)
-				? dateText
-				: edited->text.toString();
-			msgsigned->refresh(text);
-		}
-	}
-	initTime();
-}
-
 void Message::initTime() const {
 	const auto item = message();
-	if (const auto msgsigned = item->Get<HistoryMessageSigned>()
-		; msgsigned && !msgsigned->isAnonymousRank) {
-		item->_timeWidth = msgsigned->maxWidth();
-	} else if (const auto sponsored = displayedSponsorBadge()) {
-		item->_timeWidth = sponsored->maxWidth();
-	} else if (const auto edited = displayedEditBadge()) {
-		item->_timeWidth = edited->maxWidth();
-	} else {
-		const auto forwarded = item->Get<HistoryMessageForwarded>();
-		if (forwarded && forwarded->imported) {
-			const auto date = base::unixtime::parse(forwarded->originalDate);
-			item->_timeText = date.toString(
-				cDateFormat() + u", "_q + cTimeFormat() + ' '
-			) + tr::lng_imported(tr::now);
-		} else {
-			item->_timeText = dateTime().toString(cTimeFormat());
-		}
-		item->_timeWidth = st::msgDateFont->width(item->_timeText);
-	}
 	if (item->_text.hasSkipBlock()) {
 		if (item->_text.updateSkipBlock(skipBlockWidth(), skipBlockHeight())) {
 			item->_textWidth = -1;

Telegram/SourceFiles/history/view/history_view_message.h
@@ -8,6 +8,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #pragma once
 
 #include "history/view/history_view_element.h"
+#include "history/view/history_view_bottom_info.h"
 #include "ui/effects/animations.h"
 #include "base/weak_ptr.h"
 
@@ -39,12 +40,6 @@ struct PsaTooltipState : public RuntimeComponent<PsaTooltipState, Element> {
 	mutable bool buttonVisible = true;
 };
 
-struct Reactions : public RuntimeComponent<Reactions, Element> {
-	void update(const base::flat_map<QString, int> &list);
-
-	Ui::Text::String text;
-};
-
 class Message : public Element, public base::has_weak_ptr {
 public:
 	Message(
@@ -56,6 +51,11 @@ public:
 	void clickHandlerPressedChanged(
 		const ClickHandlerPtr &handler,
 		bool pressed) override;
+
+	not_null<HistoryMessage*> message() const;
+
+	const HistoryMessageEdited *displayedEditBadge() const;
+	HistoryMessageEdited *displayedEditBadge();
 
 	int marginTop() const override;
 	int marginBottom() const override;
@@ -113,6 +113,9 @@ public:
 	bool toggleSelectionByHandlerClick(
 		const ClickHandlerPtr &handler) const override;
 	int infoWidth() const override;
+	bool isSignedAuthorElided() const override;
+
+	void itemDataChanged() override;
 
 	VerticalRepaintRange verticalRepaintRange() const override;
 
@@ -125,11 +128,8 @@ protected:
 private:
 	struct CommentsButton;
 
-	not_null<HistoryMessage*> message() const;
-
 	void initLogEntryOriginal();
 	void initPsa();
-	void refreshEditedBadge();
 	void fromNameUpdated(int width) const;
 
 	[[nodiscard]] bool showForwardsFromSender(
@@ -208,14 +208,11 @@ private:
 	[[nodiscard]] bool displayFastShare() const;
 	[[nodiscard]] bool displayGoToOriginal() const;
 	[[nodiscard]] ClickHandlerPtr fastReplyLink() const;
-	[[nodiscard]] const HistoryMessageEdited *displayedEditBadge() const;
-	[[nodiscard]] HistoryMessageEdited *displayedEditBadge();
 	[[nodiscard]] auto displayedSponsorBadge() const
 		-> const HistoryMessageSponsored*;
 	[[nodiscard]] bool displayPinIcon() const;
 
 	void initTime() const;
-	[[nodiscard]] int timeLeft() const;
 	[[nodiscard]] int plainMaxWidth() const;
 	[[nodiscard]] int monospaceMaxWidth() const;
 
@@ -237,6 +234,8 @@ private:
 
 	Ui::Text::String _rightBadge;
 	int _bubbleWidthLimit = 0;
+
+	BottomInfo _bottomInfo;
 
 };
 

Telegram/CMakeLists.txt
@@ -621,6 +621,8 @@ PRIVATE
     history/view/media/history_view_theme_document.cpp
     history/view/media/history_view_web_page.h
     history/view/media/history_view_web_page.cpp
+    history/view/history_view_bottom_info.cpp
+    history/view/history_view_bottom_info.h
     history/view/history_view_contact_status.cpp
     history/view/history_view_contact_status.h
     history/view/history_view_context_menu.cpp

commit 36e3b411ca7d63989d9e0f6e0ea1be498cb896cc

Add pinned message icon.
Regression was introduced in 86b91598d1.

Fixes #17489.

Telegram/SourceFiles/history/history_item.cpp
@@ -382,7 +382,7 @@ void HistoryItem::setIsPinned(bool pinned) {
 			id));
 	}
 	if (changed) {
-		history()->owner().requestItemResize(this);
+		history()->owner().notifyItemDataChange(this);
 	}
 }
 

Telegram/SourceFiles/history/view/history_view_bottom_info.cpp
@@ -159,6 +159,17 @@ void BottomInfo::paint(
 		authorEditedWidth,
 		outerWidth);
 
+	if (_data.flags & Data::Flag::Pinned) {
+		const auto &icon = inverted
+			? st->historyPinInvertedIcon()
+			: stm->historyPinIcon;
+		right -= st::historyPinWidth;
+		icon.paint(
+			p,
+			right,
+			firstLineBottom + st::historyPinTop,
+			outerWidth);
+	}
 	if (!_views.isEmpty()) {
 		const auto viewsWidth = _views.maxWidth();
 		right -= st::historyViewsSpace + viewsWidth;
@@ -367,6 +378,9 @@ QSize BottomInfo::countOptimalSize() {
 			+ _replies.maxWidth()
 			+ st::historyViewsWidth;
 	}
+	if (_data.flags & Data::Flag::Pinned) {
+		width += st::historyPinWidth;
+	}
 	_reactionsMaxWidth = countReactionsMaxWidth();
 	width += _reactionsMaxWidth;
 	return QSize(width, st::msgDateFont->height);
@@ -409,6 +423,9 @@ BottomInfo::Data BottomInfoDataFromMessage(not_null<Message*> message) {
 	}
 	if (item->isSponsored()) {
 		result.flags |= Flag::Sponsored;
+	}
+	if (item->isPinned() && message->context() != Context::Pinned) {
+		result.flags |= Flag::Pinned;
 	}
 	if (const auto msgsigned = item->Get<HistoryMessageSigned>()) {
 		 if (!msgsigned->isAnonymousRank) {

Telegram/SourceFiles/history/view/history_view_bottom_info.h
@@ -35,6 +35,7 @@ public:
 			Sending        = 0x04,
 			RepliesContext = 0x08,
 			Sponsored      = 0x10,
+			Pinned         = 0x20,
 			//Unread, // We don't want to pass and update it in Date for now.
 		};
 		friend inline constexpr bool is_flag_type(Flag) { return true; };

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -2148,10 +2148,6 @@ bool Message::toggleSelectionByHandlerClick(
 	return false;
 }
 
-bool Message::displayPinIcon() const {
-	return data()->isPinned() && !isPinnedContext();
-}
-
 bool Message::hasFromName() const {
 	switch (context()) {
 	case Context::AdminLog:

Telegram/SourceFiles/history/view/history_view_message.h
@@ -221,7 +221,6 @@ private:
 	[[nodiscard]] bool displayFastShare() const;
 	[[nodiscard]] bool displayGoToOriginal() const;
 	[[nodiscard]] ClickHandlerPtr fastReplyLink() const;
-	[[nodiscard]] bool displayPinIcon() const;
 
 	void refreshInfoSkipBlock();
 	[[nodiscard]] int plainMaxWidth() const;