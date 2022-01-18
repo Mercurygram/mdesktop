Mercurygram/mdesktop

commit 416a5edfa98f6849187132167ed577db801d648e

Fix crash in history switching.

Telegram/SourceFiles/history/history_widget.cpp
@@ -491,7 +491,10 @@ HistoryWidget::HistoryWidget(
 
 	session().data().viewResizeRequest(
 	) | rpl::start_with_next([=](not_null<HistoryView::Element*> view) {
-		if (view->data()->mainView() == view) {
+		const auto item = view->data();
+		const auto history = item->history();
+		if (item->mainView() == view
+			&& (history == _history || history == _migrated)) {
 			updateHistoryGeometry();
 		}
 	}, lifetime());
@@ -2238,6 +2241,9 @@ void HistoryWidget::setHistory(History *history) {
 		return;
 	}
 
+	const auto wasHistory = base::take(_history);
+	const auto wasMigrated = base::take(_migrated);
+
 	// Unload lottie animations.
 	const auto unloadHeavyViewParts = [](History *history) {
 		if (history) {
@@ -2246,8 +2252,8 @@ void HistoryWidget::setHistory(History *history) {
 			history->forceFullResize();
 		}
 	};
-	unloadHeavyViewParts(_history);
-	unloadHeavyViewParts(_migrated);
+	unloadHeavyViewParts(wasHistory);
+	unloadHeavyViewParts(wasMigrated);
 
 	unregisterDraftSources();
 	_history = history;

commit 56f7539f51b21d90652afa26013a3fc7a9814c6e

Fix crash in drafts saving.
Regression was introduced in 416a5edfa9.

Telegram/SourceFiles/history/history_widget.cpp
@@ -2030,7 +2030,6 @@ void HistoryWidget::showHistory(
 		_membersDropdown.destroy();
 		_scrollToAnimation.stop();
 
-		clearAllLoadRequests();
 		setHistory(nullptr);
 		_list = nullptr;
 		_peer = nullptr;
@@ -2241,10 +2240,6 @@ void HistoryWidget::setHistory(History *history) {
 		return;
 	}
 
-	const auto wasHistory = base::take(_history);
-	const auto wasMigrated = base::take(_migrated);
-
-	// Unload lottie animations.
 	const auto unloadHeavyViewParts = [](History *history) {
 		if (history) {
 			history->owner().unloadHeavyViewParts(
@@ -2252,13 +2247,19 @@ void HistoryWidget::setHistory(History *history) {
 			history->forceFullResize();
 		}
 	};
-	unloadHeavyViewParts(wasHistory);
-	unloadHeavyViewParts(wasMigrated);
-
-	unregisterDraftSources();
-	_history = history;
-	_migrated = _history ? _history->migrateFrom() : nullptr;
-	registerDraftSource();
+	if (_history) {
+		unregisterDraftSources();
+		clearAllLoadRequests();
+		const auto wasHistory = base::take(_history);
+		const auto wasMigrated = base::take(_migrated);
+		unloadHeavyViewParts(wasHistory);
+		unloadHeavyViewParts(wasMigrated);
+	}
+	if (history) {
+		_history = history;
+		_migrated = _history ? _history->migrateFrom() : nullptr;
+		registerDraftSource();
+	}
 }
 
 void HistoryWidget::unregisterDraftSources() {
@@ -7412,6 +7413,7 @@ HistoryWidget::~HistoryWidget() {
 		session().api().saveDraftToCloudDelayed(_history);
 
 		clearAllLoadRequests();
+		setHistory(nullptr);
 		unregisterDraftSources();
 	}
 	setTabbedPanel(nullptr);