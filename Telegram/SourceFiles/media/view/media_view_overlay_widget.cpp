Mercurygram/mdesktop

commit 3a60d5947c1055958877f51060cc61ca46460dad

Improve silent stories playback.

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -3698,6 +3698,7 @@ bool OverlayWidget::createStreamingObjects() {
 	_streamed->instance.setPriority(kOverlayLoaderPriority);
 	_streamed->instance.lockPlayer();
 	_streamed->withSound = _document
+		&& !_document->isSilentVideo()
 		&& (_document->isAudioFile()
 			|| _document->isVideoFile()
 			|| _document->isVoiceMessage()
@@ -4038,12 +4039,14 @@ void OverlayWidget::restartAtSeekPosition(crl::time position) {
 	};
 	if (!_streamed->withSound) {
 		options.mode = Streaming::Mode::Video;
-		options.loop = true;
+		options.loop = !_stories;
 	} else {
 		Assert(_document != nullptr);
 		const auto messageId = _message ? _message->fullId() : FullMsgId();
 		options.audioId = AudioMsgId(_document, messageId);
-		options.speed = Core::App().settings().videoPlaybackSpeed();
+		options.speed = _stories
+			? Core::App().settings().videoPlaybackSpeed()
+			: 1.;
 		if (_pip) {
 			_pip = nullptr;
 		}
@@ -4111,7 +4114,7 @@ void OverlayWidget::playbackControlsSpeedChanged(float64 speed) {
 		Core::App().settings().setVideoPlaybackSpeed(speed);
 		Core::App().saveSettingsDelayed();
 	}
-	if (_streamed && _streamed->controls) {
+	if (_streamed && _streamed->controls && !_stories) {
 		DEBUG_LOG(("Media playback speed: %1 to _streamed.").arg(speed));
 		_streamed->instance.setSpeed(speed);
 	}

commit 5dc7ab3bee77596febfe224bfce03b271a8b5fe8

Fix initial video playback speed.
Regression was introduced in 3a60d5947c.

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -4051,8 +4051,8 @@ void OverlayWidget::restartAtSeekPosition(crl::time position) {
 		const auto messageId = _message ? _message->fullId() : FullMsgId();
 		options.audioId = AudioMsgId(_document, messageId);
 		options.speed = _stories
-			? Core::App().settings().videoPlaybackSpeed()
-			: 1.;
+			? 1.
+			: Core::App().settings().videoPlaybackSpeed();
 		if (_pip) {
 			_pip = nullptr;
 		}