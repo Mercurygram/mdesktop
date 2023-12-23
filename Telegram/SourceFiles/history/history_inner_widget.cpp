Mercurygram/mdesktop

commit 690f5f760443ec72fb5714f9d617bb01eaa29fe1

Fixed display of menu items from sponsored message with selected text.

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -2526,6 +2526,9 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 			? link->copyToClipboardContextItemText()
 			: QString();
 
+		if (item->isSponsored()) {
+			FillSponsoredMessagesMenu(controller, item->fullId(), _menu);
+		}
 		if (isUponSelected > 0) {
 			addReplyAction(item);
 			const auto selectedText = getSelectedText();
@@ -2587,10 +2590,6 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 							QGuiApplication::clipboard()->setText(phone);
 						}, &st::menuIconCopy);
 					}
-				}
-				if (item->isSponsored()) {
-					const auto itemId = item->fullId();
-					FillSponsoredMessagesMenu(controller, itemId, _menu);
 				}
 				if (!item->isService() && view && actionText.isEmpty()) {
 					if (!hasCopyRestriction(item)

commit 8866c36e19307aa708f24e54008cb6ae7d83840f

Fix crash in context menu.
Regression was introduced in 690f5f7604.

Fixes #27254.

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -2526,7 +2526,7 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 			? link->copyToClipboardContextItemText()
 			: QString();
 
-		if (item->isSponsored()) {
+		if (item && item->isSponsored()) {
 			FillSponsoredMessagesMenu(controller, item->fullId(), _menu);
 		}
 		if (isUponSelected > 0) {